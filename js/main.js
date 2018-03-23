e074b06be76e6aabe5688b178d67844244178501
Meenakshi Kunduru <kmreddi03@gmail.com>
2018-03-23 19:34:34 -0400

    Change fonts and make the fonts consistent for all sizes

--- js/main.js
+++ js/main.js
@@ -37,6 +37,39 @@ $(document).ready(function() {
 
         e.preventDefault();
     });
+
+//    var getUrlParameter = function getUrlParameter(sParam) {
+//        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+//            sURLVariables = sPageURL.split('&'),
+//            sParameterName,
+//            i;
+//
+//        for (i = 0; i < sURLVariables.length; i++) {
+//            sParameterName = sURLVariables[i].split('=');
+//
+//            if (sParameterName[0] === sParam) {
+//                return sParameterName[1] === undefined ? true : sParameterName[1];
+//            }
+//        }
+//    };
+//
+//    console.log("Got here before the call");
+//    var camp = getUrlParameter('camp');
+//    console.log("camp: "+camp);
+//    if(camp == "discovery") {
+//        $('.contact-subject').val("I am interested to know more about the discovery camp");
+//    }
+//
+//    $('.camps .button').on('click', function(){
+//        var data = $(this).attr('data-camp');
+//        window.location.replace("contact.html?camp="+data+"#contactus");
+//    })
+
+        $('#contactus .button').on('click', function(){
+            $('#contactus input').val("");
+            $('#contactus textarea').val("");
+            window.location.replace("contact.html#contactus");
+        });
 });
 
 
